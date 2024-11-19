dbbe47cc524790476ff75b250318cdd5e7ff1366
JohnoAlm <almenoliver02@gmail.com>
2024-11-19 15:36:16 +0100

    Added factories for all entities.

---
+++ PaymentProvider/Factories/Entities/InvoiceEntityFactory.cs
@@ -0,0 +1,19 @@
+﻿using PaymentProvider.Entities;
+
+namespace PaymentProvider.Factories.Entities;
+
+public static class InvoiceEntityFactory
+{
+    public static InvoiceEntity Create(string fullName, string streetAddress, string city, string postalCode, string country, string paymentOptions)
+    {
+        return new InvoiceEntity
+        {
+            FullName = fullName,
+            StreetAddress = streetAddress,
+            City = city,
+            PostalCode = postalCode,
+            Country = country,
+            PaymentOption = paymentOptions
+        };
+    }
+}
---
+++ PaymentProvider/Factories/Entities/ProductEntityFactory.cs
@@ -0,0 +1,17 @@
+﻿using PaymentProvider.Entities;
+
+namespace PaymentProvider.Factories.Entities;
+
+public static class ProductEntityFactory
+{
+    public static ProductEntity Create(string name, int amount, decimal price, decimal discountedPrice)
+    {
+        return new ProductEntity
+        {
+            Name = name,
+            Amount = amount,
+            Price = price,
+            DiscountedPrice = discountedPrice
+        };
+    }
+}
