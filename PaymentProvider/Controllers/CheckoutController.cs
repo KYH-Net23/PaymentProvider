c176c62490e59098335da061b85b9eb1552bb42c
Hossén Rahimzadegan <maulvids@gmail.com>
2024-11-26 10:49:15 +0100

    Fixed order (and its other properties) duplicates being created for the most part when

--- PaymentProvider/Controllers/CheckoutController.cs
+++ PaymentProvider/Controllers/CheckoutController.cs
@@ -21,40 +21,49 @@ namespace PaymentProvider.Controllers
             {
                 if (!ModelState.IsValid) BadRequest();
                 if (orderDetails == null) return NotFound();
+                var service = new SessionService();
+                var session = new Session();
                 var orderEntity = OrderEntityFactory.Create(orderDetails);
-                await _orderService.CreateOrderAsync(orderEntity);
+                orderEntity = await _orderService.CreateOrderAsync(orderEntity);
                 _orderEntityId = orderEntity.Id;
                 //var domain = $"{Request.Scheme}://{Request.Host}";
                 var domain = "http://localhost:5173";
                 var customer = _stripeCustomerService.CreateCustomer(orderDetails);
-                var options = new SessionCreateOptions
+                if (!string.IsNullOrEmpty(orderEntity.SessionId))
                 {
-                    UiMode = "embedded",
-                    Currency = "usd",
-                    LineItems = _orderService.GetOrderItemsList(orderDetails),
-                    Mode = "payment",
-                    //ReturnUrl = domain + "/return?session_id={CHECKOUT_SESSION_ID}",
-                    ReturnUrl = domain + "/orderconfirmation",
-                    Customer = customer.Id,
-                    ShippingOptions =
-                    [
-                        _orderService.GetShippingOption(orderDetails)
-                    ],
-                    InvoiceCreation = new SessionInvoiceCreationOptions
+                    session = await service.GetAsync(orderEntity.SessionId);
+                    session.Customer = customer;
+                }
+                else
+                {
+                    var options = new SessionCreateOptions
                     {
-                        Enabled = true
-                    },
-                    Metadata = new Dictionary<string, string>
-                    {
-                        {"orderId", $"{orderEntity.Id}" }
-                    },
-                    BillingAddressCollection = "required",
-                };
-                var service = new SessionService();
-                var session = service.Create(options);
-                session.Customer = customer;
-                orderEntity.SessionId = session.Id;
-                await _orderService.SaveChangesAsync();
+                        UiMode = "embedded",
+                        Currency = "usd",
+                        LineItems = _orderService.GetOrderItemsList(orderDetails),
+                        Mode = "payment",
+                        //ReturnUrl = domain + "/return?session_id={CHECKOUT_SESSION_ID}",
+                        ReturnUrl = domain + "/orderconfirmation",
+                        Customer = customer.Id,
+                        ShippingOptions =
+                        [
+                            _orderService.GetShippingOption(orderDetails)
+                        ],
+                        InvoiceCreation = new SessionInvoiceCreationOptions
+                        {
+                            Enabled = true
+                        },
+                        Metadata = new Dictionary<string, string>
+                        {
+                            {"orderId", $"{orderEntity.Id}" }
+                        },
+                        BillingAddressCollection = "required",
+                    };
+                    session = service.Create(options);
+                    session.Customer = customer;
+                    orderEntity.SessionId = session.Id;
+                    await _orderService.SaveChangesAsync();
+                }
                 return Ok(new { sessionId = session.Id, clientSecret = session.ClientSecret, orderId = _orderEntityId });
             }
             catch (Exception ex)
--- PaymentProvider/Services/OrderService.cs
+++ PaymentProvider/Services/OrderService.cs
@@ -5,6 +5,7 @@ using PaymentProvider.Entities;
 using PaymentProvider.Models;
 using Stripe.Checkout;
 using Stripe.Climate;
+using System;
 
 namespace PaymentProvider.Services
 {
@@ -12,10 +13,22 @@ namespace PaymentProvider.Services
     {
         private readonly RikaOrdersDbContext _context = context;
 
-        public async Task CreateOrderAsync(OrderEntity order)
+        public async Task<OrderEntity> CreateOrderAsync(OrderEntity order)
         {
             try
             {
+                // Comparison logic checks to prevent duplicates
+
+                // check if order with every detail already exists
+                var existingOrder = await FindOrCreateMatchingOrderAsync(order);
+                if (existingOrder != null && existingOrder != order)
+                {
+                    existingOrder.Date = DateTime.UtcNow;
+                    await _context.SaveChangesAsync();
+                    return existingOrder;
+                }
+
+                // check if shipping details already exists
                 var existingShipping = await _context.ShippingDetails
                     .FirstOrDefaultAsync(s => s.TrackingLink == order.Shipping.TrackingLink
                     && s.OrderArrival == order.Shipping.OrderArrival
@@ -26,6 +39,7 @@ namespace PaymentProvider.Services
                 {
                     order.Shipping = existingShipping;
                 }
+                // check if customers details already exists
                 var existingCustomer = await _context.CustomerDeliveryInformation
                     .FirstOrDefaultAsync(c => c.StreetAddress == order.Shipping.CustomerDeliveryInformation.StreetAddress
                     && c.PostalCode == order.Shipping.CustomerDeliveryInformation.PostalCode
@@ -37,28 +51,90 @@ namespace PaymentProvider.Services
                 {
                     order.Shipping.CustomerDeliveryInformation = existingCustomer;
                 }
+                // check if postal agent details already exists
+                var existingPostalAgent = await _context.PostalAgentDeliveryInformation
+                    .FirstOrDefaultAsync(p => p.StreetAddress == order.Shipping.PostalAgentDeliveryInformation.StreetAddress
+                    && p.PostalCode == order.Shipping.PostalAgentDeliveryInformation.PostalCode
+                    && p.PostalAgentName == order.Shipping.PostalAgentDeliveryInformation.PostalAgentName
+                    && p.City == order.Shipping.PostalAgentDeliveryInformation.City
+                    && p.Country == order.Shipping.PostalAgentDeliveryInformation.Country
+                    && p.PhoneNumber == order.Shipping.PostalAgentDeliveryInformation.PhoneNumber);
+                if (existingPostalAgent != null)
+                {
+                    order.Shipping.PostalAgentDeliveryInformation = existingPostalAgent;
+                }
 
-                //var matchedProducts = new List<ProductEntity>();
-                //foreach (var product in order.Products)
-                //{
-                //    var matchingProduct = await _context.Products.
-                //        FirstOrDefaultAsync(p => p.ProductId == product.ProductId
-                //        && p.Amount == product.Amount
-                //        && p.Price == product.Price
-                //        && p.DiscountedPrice == product.DiscountedPrice
-                //        && p.Size == product.Size);
-                //    if (matchingProduct != null)
-                //    {
-                //        matchedProducts.Add(matchingProduct);
-                //    }
-                //}
-                //order.Products = matchedProducts;
                 await _context.Orders.AddAsync(order);
                 await _context.SaveChangesAsync();
+                return order;
             }
-            catch { }
+            catch { return null!; }
         }
 
+        private async Task<OrderEntity> FindOrCreateMatchingOrderAsync(OrderEntity order)
+        {
+            var existingOrder = await _context.Orders
+                .Include(o => o.Products)
+                .Include(o => o.Invoice)
+                .Include(o => o.Shipping)
+                .ThenInclude(s => s.CustomerDeliveryInformation)
+                .Include(o => o.Shipping)
+                .ThenInclude(s => s.PostalAgentDeliveryInformation)
+                .FirstOrDefaultAsync(o =>
+                o.Date.Date == order.Date.Date
+                && o.ReceivingEmail == order.ReceivingEmail
+                && o.OrderTotal == order.OrderTotal
+                );
+            if (existingOrder != null && AreOrdersEqual(order, existingOrder)) return existingOrder;
+
+            return order;
+        }
+        public bool AreOrdersEqual(OrderEntity order1, OrderEntity order2)
+        {
+            if (order1 == null || order2 == null) return false;
+
+            //if (obj1.Invoice.PostalCode != obj2.Invoice.PostalCode 
+            //    || obj1.Invoice.FullName != obj2.Invoice.FullName
+            //    || obj1.Invoice.City != obj2.Invoice.City
+            //    || obj1.Invoice.Country != obj2.Invoice.Country
+            //    || obj1.Invoice.City != obj2.Invoice.City) return false;            
+
+            // products comparison
+            foreach (var product1 in order1.Products)
+            {
+                foreach (var product2 in order2.Products)
+                {
+                    if (product1.ProductId != product2.ProductId
+                        || product1.Brand != product2.Brand
+                        || product1.Model != product2.Model
+                        || product1.Amount != product2.Amount
+                        || product1.Description != product2.Description
+                        || product1.Price != product2.Price
+                        || product1.DiscountedPrice != product2.DiscountedPrice
+                        || product1.Category != product2.Category
+                        || product1.Size != product2.Size
+                        || product1.ImageUrl != product2.ImageUrl) return false;
+                }
+            }
+            // shipping.customerdelivery comparison
+            if (order1.Shipping.CustomerDeliveryInformation.FullName != order2.Shipping.CustomerDeliveryInformation.FullName
+                || order1.Shipping.CustomerDeliveryInformation.StreetAddress != order2.Shipping.CustomerDeliveryInformation.StreetAddress
+                || order1.Shipping.CustomerDeliveryInformation.City != order2.Shipping.CustomerDeliveryInformation.City
+                || order1.Shipping.CustomerDeliveryInformation.PostalCode != order2.Shipping.CustomerDeliveryInformation.PostalCode
+                || order1.Shipping.CustomerDeliveryInformation.Country != order2.Shipping.CustomerDeliveryInformation.Country
+                || order1.Shipping.CustomerDeliveryInformation.PhoneNumber != order2.Shipping.CustomerDeliveryInformation.PhoneNumber) return false;
+            // shipping.postalagent comparison
+            if (order1.Shipping.PostalAgentDeliveryInformation.PostalAgentName != order2.Shipping.PostalAgentDeliveryInformation.PostalAgentName
+                || order1.Shipping.PostalAgentDeliveryInformation.StreetAddress != order2.Shipping.PostalAgentDeliveryInformation.StreetAddress
+                || order1.Shipping.PostalAgentDeliveryInformation.City != order2.Shipping.PostalAgentDeliveryInformation.City
+                || order1.Shipping.PostalAgentDeliveryInformation.PostalCode != order2.Shipping.PostalAgentDeliveryInformation.PostalCode
+                || order1.Shipping.PostalAgentDeliveryInformation.Country != order2.Shipping.PostalAgentDeliveryInformation.Country
+                || order1.Shipping.PostalAgentDeliveryInformation.PhoneNumber != order2.Shipping.PostalAgentDeliveryInformation.PhoneNumber) return false;
+            // shipping comparison
+            if (order1.Shipping.OrderArrival != order2.Shipping.OrderArrival
+                || order1.Shipping.ShippingCost != order2.Shipping.ShippingCost) return false;
+            return true;
+        }
         public async Task Delete(OrderEntity order)
         {
             try
